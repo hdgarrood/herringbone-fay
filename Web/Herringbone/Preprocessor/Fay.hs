e432324240348845fa99dd6eede05dfda1cbd607
Harry Garrood <hdgarrood@gmail.com>
2014-01-02 17:56:42 +0000

    IT WORKS! :D

--- Web/Herringbone/Preprocessor/Fay.hs
+++ Web/Herringbone/Preprocessor/Fay.hs
@@ -6,20 +6,25 @@ import qualified Data.ByteString as B
 import System.IO
 import System.IO.Temp
 import qualified Fay
+import qualified Fay.Compiler.Config as Fay
 import Web.Herringbone
 
 compile :: Fay.CompileConfig -> ByteString -> IO (Either CompileError ByteString)
 compile config inputData = do
-    withSystemTempFile "herringbone-fay.hs" $ \path h -> do
-        B.hPutStr h inputData
-        hClose h
+    withSystemTempDirectory "herringbone-fay" $ \tmpDir -> do
+        let path = tmpDir ++ "/HelloWorld.hs"
+        withFile path WriteMode $ \h -> do
+            B.hPutStr h inputData
+            hClose h
 
-        result <- Fay.compileFile config path
-        either (return . Left . toBS . Fay.showCompileError)
+        let config' = Fay.addConfigDirectoryIncludePaths [tmpDir] config
+        result <- Fay.compileFile config' path
+        either (return . Left . toBS . wrapWithPre . Fay.showCompileError)
                (return . Right . toBS . fst)
                result
     where
         toBS = B.pack . map (fromIntegral . ord)
+        wrapWithPre str = "<pre>" ++ str ++ "</pre>"
 
 fay :: Fay.CompileConfig -> PP
 fay config = PP
